66754af130b1ac1a1bf65404f4b9b6f56917d779
anorse <AaronNorris@gmail.com>
2015-08-05 20:48:46 -0600

    Example baud rates changed to 9600 for use with Addicore's GPS unit.

--- TinyGPSPlus/examples/SatElevTracker/SatElevTracker.ino
+++ TinyGPSPlus/examples/SatElevTracker/SatElevTracker.ino
@@ -9,8 +9,8 @@
    It requires the use of SoftwareSerial and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
 */
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 4800;
+static const int RXPin = 3, TXPin = 2;
+static const uint32_t GPSBaud = 9600;
 static const int MAX_SATELLITES = 40;
 static const int PAGE_LENGTH = 40;
 
